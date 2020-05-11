842efed162dc16bd161a21421c7dc81eaf75c917
Hoon <hoon.jang@navercorp.com>
2020-05-11 16:55:02 +0900

    Initial commit

---
+++ app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.naver.hackday2020.mapclustering
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
