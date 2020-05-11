91c88a2c08bb08219b4830cc70cd722269951b84
s2choco <kyj_0129@naver.com>
2020-05-11 22:40:32 +0900

    지도 fragment 추가

--- app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
@@ -2,11 +2,21 @@ package com.naver.hackday2020.mapclustering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        (map as MapFragment?) ?: MapFragment.newInstance().also {
+            supportFragmentManager.beginTransaction().add(R.id.map, it).commit()
+        }
     }
 }
