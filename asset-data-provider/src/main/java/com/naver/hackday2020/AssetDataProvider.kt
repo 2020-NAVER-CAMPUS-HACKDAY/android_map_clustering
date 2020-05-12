3f2d59886a13fdd572423e25784876c6a71041aa
zion830 <zion830@honglab.org>
2020-05-12 21:06:52 +0900

    모듈 이름을 asset-data-provider로 변경
    geo보다 더 범용적으로 적용 가능한 이름으로 변경함

--- asset-data-provider/src/main/java/com/naver/hackday2020/AssetDataProvider.kt
+++ asset-data-provider/src/main/java/com/naver/hackday2020/AssetDataProvider.kt
@@ -8,6 +8,9 @@ import java.nio.charset.StandardCharsets
 class AssetDataProvider(private val context: Context) {
     var charset: String = StandardCharsets.UTF_8.name()
 
+    /**
+     * json 파일의 내용을 읽고 [T] 타입으로 변환합니다.
+     */
     @WorkerThread
     fun <T> receiveData(
         assetFileName: String,
@@ -17,6 +20,10 @@ class AssetDataProvider(private val context: Context) {
         return JsonConverter.jsonToObject(resultTypeSerializer, content)
     }
 
+    /**
+     * json 파일의 내용을 읽고 [T] 타입으로 변환합니다.
+     * 백그라운드에서 변환 처리 후, 실행 결과를 [OnDataLoadedCallback]에서 사용할 수 있습니다.
+     */
     fun <T> receiveDataAsync(
         assetFileName: String,
         resultTypeSerializer: DeserializationStrategy<T>,
