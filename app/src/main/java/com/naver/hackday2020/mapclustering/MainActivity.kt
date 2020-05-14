e5beae629bcc5d384f918e8aba8fbd174cdbc303
zion830 <zion830@honglab.org>
2020-05-14 14:59:44 +0900

    setUpMarkers 추가

--- app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.naver.hackday2020.mapclustering
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (map as MapFragment).getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+
+    }
+
+    private fun setUpMarkers(markers: List<Marker>, naverMap: NaverMap) {
+        for (marker in markers) {
+            marker.map = naverMap
+        }
     }
 }
--- app/src/main/java/com/naver/hackday2020/mapclustering/model/PlaceInfo.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/model/PlaceInfo.kt
@@ -1,8 +1,6 @@
 package com.naver.hackday2020.mapclustering.model
 
 
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.overlay.Marker
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -10,9 +8,4 @@ import kotlinx.serialization.Serializable
 data class PlaceInfo(
     @SerialName("places")
     val places: List<Place> = listOf()
-) {
-
-    fun getMarkers(): List<Marker> = places.map { item ->
-        Marker(LatLng(item.y.toDouble(), item.x.toDouble()))
-    }
-}
+)
