714c522a023d929ca421355964e51e301e6e600c
zion830 <zion830@honglab.org>
2020-05-16 21:04:34 +0900

    PlaceUiModel -> NaverPlaceItem 이름 변경

--- app/src/main/java/com/naver/hackday2020/mapclustering/ui/NaverPlaceItem.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/ui/NaverPlaceItem.kt
@@ -3,27 +3,34 @@ package com.naver.hackday2020.mapclustering.ui
 import android.util.Log
 import com.naver.hackday2020.mapclustering.clustering.ClusterItem
 import com.naver.hackday2020.mapclustering.model.Place
+import com.naver.hackday2020.mapclustering.model.PlaceList
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.overlay.Marker
 
-class PlaceUiModel(
+class NaverPlaceItem(
     val place: Place
 ) : ClusterItem {
     val marker: Marker
 
     companion object {
-        private val tag = PlaceUiModel::class.java.name
+        private val tag = NaverPlaceItem::class.java.name
 
-        fun from(place: Place) = PlaceUiModel(place)
+        fun from(place: Place) = NaverPlaceItem(place)
+
+        fun fromList(placeList: PlaceList) = placeList.places.map { from(it) }
     }
 
     override fun getPosition() = LatLng(place.y, place.x)
 
-    fun setOnClickListener(onClick: (item: PlaceUiModel) -> Unit) {
+    fun setOnClickListener(onClick: (item: NaverPlaceItem) -> Unit) {
         marker.setOnClickListener {
             onClick(this)
             true
         }
+    }
+
+    fun setVisibility(isVisible: Boolean) {
+        marker.isVisible = isVisible
     }
 
     init {
