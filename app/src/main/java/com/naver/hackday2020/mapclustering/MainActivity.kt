14405ae07fe4d8d67d2ed91173e1a07d15861c95
zion830 <zion830@honglab.org>
2020-05-16 20:56:19 +0900

    MainActivity에서 ClusterManager 적용

--- app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
@@ -2,7 +2,10 @@ package com.naver.hackday2020.mapclustering
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.naver.maps.map.MapFragment
+import com.naver.hackday2020.mapclustering.clustering.ClusterManager
+import com.naver.hackday2020.mapclustering.ext.showSnack
+import com.naver.hackday2020.mapclustering.model.PlaceDataProvider
+import com.naver.hackday2020.mapclustering.ui.NaverPlaceItem
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
@@ -13,12 +16,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        (map as MapFragment).getMapAsync(this)
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-
+        PlaceDataProvider.getAllData(
+            success = {
+                ClusterManager<NaverPlaceItem>(naverMap).apply {
+                    addItems(NaverPlaceItem.fromList(it))
+                }
+            },
+            failed = {
+                layout_map.showSnack(R.string.loading_error_msg)
+            }
+        )
     }
 
     private fun setUpMarkers(markers: List<Marker>, naverMap: NaverMap) {
---
+++ app/src/main/java/com/naver/hackday2020/mapclustering/ext/ViewExt.kt
@@ -0,0 +1,28 @@
+package com.naver.hackday2020.mapclustering.ext
+
+import android.view.View
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.naver.hackday2020.mapclustering.R
+
+fun View.showSnack(
+    @StringRes resId: Int,
+    length: Int = Snackbar.LENGTH_LONG,
+    @ColorRes textColor: Int? = null
+) {
+    showSnack(context.getString(resId), length, textColor)
+}
+
+fun View.showSnack(
+    message: String,
+    length: Int = Snackbar.LENGTH_LONG,
+    @ColorRes textColor: Int? = null
+) {
+    Snackbar.make(this, message, length).run {
+        textColor?.let { setActionTextColor(ContextCompat.getColor(context, textColor)) }
+        setAction(context.getString(R.string.ok)) { dismiss() }
+        show()
+    }
+}
