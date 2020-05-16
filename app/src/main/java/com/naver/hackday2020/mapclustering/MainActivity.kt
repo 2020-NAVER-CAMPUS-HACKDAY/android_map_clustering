ef804af3d8ef6f2ff3273d289f3a8abcf84f2fb0
zion830 <zion830@honglab.org>
2020-05-16 21:17:26 +0900

    getMapAsync 추가

--- app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/MainActivity.kt
@@ -6,6 +6,7 @@ import com.naver.hackday2020.mapclustering.clustering.ClusterManager
 import com.naver.hackday2020.mapclustering.ext.showSnack
 import com.naver.hackday2020.mapclustering.model.PlaceDataProvider
 import com.naver.hackday2020.mapclustering.ui.NaverPlaceItem
+import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (map as MapFragment).getMapAsync(this)
     }
 
     override fun onMapReady(naverMap: NaverMap) {
