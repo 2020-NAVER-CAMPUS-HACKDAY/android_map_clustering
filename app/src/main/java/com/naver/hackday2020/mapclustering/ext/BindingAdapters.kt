54bfdb9b397e10332ca978fbfaaed4d7ae233e68
s2choco <kyj_0129@naver.com>
2020-05-18 20:17:12 +0900

    디버깅용 Log 제거

--- app/src/main/java/com/naver/hackday2020/mapclustering/ext/BindingAdapters.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/ext/BindingAdapters.kt
@@ -1,6 +1,5 @@
 package com.naver.hackday2020.mapclustering.ext
 
-import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
@@ -9,7 +8,6 @@ import com.naver.hackday2020.mapclustering.R
 
 @BindingAdapter("bind_image")
 fun ImageView.bindImage(url: String?) {
-    Log.d("MapApp", "url = $url")
     Glide.with(context)
         .load(url)
         .error(R.drawable.no_image)
