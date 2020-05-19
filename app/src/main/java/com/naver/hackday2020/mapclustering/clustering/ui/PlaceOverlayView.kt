32b25f3e490f3e60c61bf379564a07dcec6ef3be
zion830 <zion830@honglab.org>
2020-05-19 21:26:50 +0900

    마커 커스텀 적용

--- app/src/main/java/com/naver/hackday2020/mapclustering/clustering/ui/PlaceOverlayView.kt
+++ app/src/main/java/com/naver/hackday2020/mapclustering/clustering/ui/PlaceOverlayView.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import com.naver.hackday2020.mapclustering.MapApp
 import com.naver.hackday2020.mapclustering.databinding.LayoutImageMarkerBinding
+import com.naver.hackday2020.mapclustering.ui.MarkerType
+import com.naver.hackday2020.mapclustering.ui.NaverPlaceItem
 
 class PlaceOverlayView @JvmOverloads constructor(
     context: Context,
@@ -12,10 +15,22 @@ class PlaceOverlayView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
     private val binding = LayoutImageMarkerBinding.inflate(LayoutInflater.from(context))
+    private var markerType: MarkerType = MarkerType.RESTAURANT
+        set(value) {
+            field = value
+            binding.ivPlaceType.apply {
+                setImageDrawable(markerType.getIconDrawable())
+            }
+        }
 
     init {
-        addView(binding.root.apply {
-            layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        })
+        addView(binding.root)
+    }
+
+    companion object {
+
+        fun create(placeItem: NaverPlaceItem) = PlaceOverlayView(MapApp.appContext).apply {
+            markerType = MarkerType.of(placeItem.place.category)
+        }
     }
 }
